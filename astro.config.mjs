83a56924c5f5af068f5aac955870d7e32a15dc5f
TecnoConecta <ltinunido@gmail.com>
2024-04-18 13:03:36 -0600

    deploy commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,43 +1,27 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
 import compressor from "astro-compressor";
 import sitemap from "@astrojs/sitemap";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }), 
   site: "https://tecnoconecta.net",
-  integrations: [
-    react({
-      include: ["**/react/*"],
-    }),
-    tailwind({
-      configFile: "./tailwind.config.js",
-    }),
-    compressor({
-      gzip: true,
-      brotli: false,
-      fileExtensions: [
-        ".css",
-        ".js",
-        ".html",
-        ".xml",
-        ".cjs",
-        ".mjs",
-        ".svg",
-        ".txt",
-        ".webp",
-      ],
-    }),
-    sitemap({
-      changefreq: "weekly",
-      priority: 0.7,
-      lastmod: new Date("2024-04-15"),
-    }),
-  ],
+  integrations: [react({
+    include: ["**/react/*"]
+  }), tailwind({
+    configFile: "./tailwind.config.js"
+  }), compressor({
+    gzip: true,
+    brotli: false,
+    fileExtensions: [".css", ".js", ".html", ".xml", ".cjs", ".mjs", ".svg", ".txt", ".webp"]
+  }), sitemap({
+    changefreq: "weekly",
+    priority: 0.7,
+    lastmod: new Date("2024-04-15")
+  })],
+  output: "server",
+  adapter: vercel()
 });
